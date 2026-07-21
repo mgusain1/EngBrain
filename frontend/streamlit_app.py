c6805d2aa0bac1545495077f24562666781e5051
Madhur Gusain <62810981+mgusain1@users.noreply.github.com>
2026-07-21 18:13:17 -0500

    frontend deployed

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import uuid
 
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://engbrain-api.onrender.com"
 
 st.set_page_config(
     page_title="EngBrain",
